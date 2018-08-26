bd5e14a250a08a96e7b17a2a0d2b3375bdb3d753
NekOrz <joe1256joe@gmail.com>
2018-08-26 06:37:07 -0700

    setMiddle.js version 0.1

--- setMiddle.js
+++ setMiddle.js
@@ -1,2 +1,37 @@
+const fs = require('fs');
+const DOMParser = new (require('xmldom').DOMParser)();
+const XMLSerializer = new (require('xmldom').XMLSerializer)();
 
+const Config = require('./config.json');
 
+const OutputPath = __dirname + "/output";
+const RawOutputPath = OutputPath + "/raw_0";
+const RawDataListPath = RawOutputPath + "/list.json";
+
+const dataList = require(RawDataListPath);
+
+class PrtlMiddler {
+    constructor() {
+        console.log("Setting prtl middle...");
+        for (let i of dataList) {
+            const prtl = fs.readFileSync(RawOutputPath + "/" + i + "_raw.prtl", 'utf16le');
+            const dom = DOMParser.parseFromString(prtl);
+            const textChains = dom.getElementsByTagName('TextChain');
+            const width = textChains[0].getElementsByTagName('Size')[0].getElementsByTagName('x')[0].textContent;
+
+            textChains[0].getElementsByTagName('Position')[0].getElementsByTagName('x')[0].textContent = ((1280.0 - width) / 2.0).toFixed(2);
+            const XPoses = textChains[0].getElementsByTagName('XPos');
+            for (let j = 0; j < XPoses.length; j++) {
+                console.log(XPoses[j].textContent + " " + j);
+                XPoses[j].textContent = ((1280.0 - width) / 2.0).toFixed(2);
+            }
+
+            let output = XMLSerializer.serializeToString(dom);
+
+            const filePath = OutputPath + "/" + i + ".prtl";
+            fs.writeFileSync(filePath, output, 'utf16le');
+        }
+    }
+}
+
+let Middler = new PrtlMiddler();
